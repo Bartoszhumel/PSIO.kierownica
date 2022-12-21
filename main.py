626c99bda356121078aea2500f5e69654015f25b
Bartosz Humel <235876@edu.p.lodz.pl>
2022-12-21 09:19:46 +0100

    zczytuje ruch z calego wideo ale rysuje krawedzie tylko dla obszaru z kierownicą ale jest przesuniete w lewy gorny rog

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import time
 device=0
 cap = cv2.VideoCapture('HG16MEU.mp4')
 pos_frame = 0
-cap.set(cv2.CAP_PROP_FRAME_WIDTH,640)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH,720)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT,480)
 while not cap.isOpened():
     cap = cv2.VideoCapture('HG16MEU.mp4')
@@ -14,6 +14,7 @@ while not cap.isOpened():
     print ("Czekam na wideo")
 break_flag=False
 start_flag=True
+previous_frame = None
 while True:
     t = time.time()
     for i in range(25):
@@ -22,16 +23,63 @@ while True:
             # ramka jest gotowa
             pos_frame = cap.get(cv2.CAP_PROP_POS_FRAMES)
             obraz_sz = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            obraz_fil = cv2.GaussianBlur(obraz_sz, (5, 5), 0)
-            obraz_kr = cv2.Canny(obraz_fil, 60, 150)
+            obraz_fil = cv2.GaussianBlur(obraz_sz, (15, 15), 0)
+            # obraz_kr = cv2.Canny(obraz_sz, 60, 150)
+            circles=cv2.HoughCircles(obraz_fil, cv2.HOUGH_GRADIENT, 1, 20,minRadius=100,maxRadius=700)
+            # object_detector = cv2.createBackgroundSubtractorMOG2()
+            # x= object_detector.apply(frame)
+
+
+
+            if circles is not None:
+                maks=0
+                x1=0
+                y1=0
+                r1=0
+                # convert the (x, y) coordinates and radius of the circles to integers
+                circles = np.round(circles[0, :]).astype("int")
+                # loop over the (x, y) coordinates and radius of the circles
+                for (x, y, r) in circles:
+                    if(r>maks):
+                        maks=r
+                        x1=x
+                        y1=y
+                        r1=r
+
+                kierownicaobrys=cv2.circle(frame, (x1, y1), r1, (0, 255, 0), 4)
+                cv2.rectangle(frame, (x1 - 5, y1 - 5), (x1 + 5, y1 + 5), (0, 128, 255), -1)
+                # cv2.imshow('image', kierownica)
+                # cv2.waitKey(1000)
+
+                ################################-------------------------------------------------------------------------------wykrywanie ruchu
+                if (previous_frame is None):
+                        # First frame; there is no previous one yet
+                    previous_frame = obraz_fil
+                    continue
+                # calculate difference and update previous frame
+                diff_frame = cv2.absdiff(src1=previous_frame, src2=obraz_fil)
+                previous_frame = obraz_fil
+
+                    # 4. Dilute the image a bit to make differences more seeable; more suitable for contour detection
+                kernel = np.ones((5, 5))
+                diff_frame = cv2.dilate(diff_frame, kernel, 1)
+
+                    # 5. Only take different areas that are different enough (>20 / 255)
+                thresh_frame = cv2.threshold(src=diff_frame, thresh=20, maxval=255, type=cv2.THRESH_BINARY)[1]
+
+                kierownica= thresh_frame[y1-r1:(y1+2*r1), x1-r1:(x1+2*r1)]
+
+
+                contours, _ = cv2.findContours(image=kierownica, mode=cv2.RETR_EXTERNAL,
+                                                   method=cv2.CHAIN_APPROX_SIMPLE)
+                cv2.drawContours(image=frame, contours=contours, contourIdx=-1, color=(0, 255, 0), thickness=2,
+                                         lineType=cv2.LINE_AA)
+                    ################################-----------------------------------------------------------------------------------------------------------
+
             cv2.imshow("Obraz", frame)
-            #cv2.imshow("Filtr", obraz_fil)
-            cv2.imshow("krawedzie", obraz_kr)
             if start_flag:
                 cv2.setWindowProperty('Obraz',cv2.WND_PROP_TOPMOST,1)
                 cv2.moveWindow('Obraz',20,20)
-                cv2.setWindowProperty('krawedzie',cv2.WND_PROP_TOPMOST,1)
-                cv2.moveWindow('krawedzie',800,20)
                 start_flag=False
         else:
             # ramka nie jest gotowa
